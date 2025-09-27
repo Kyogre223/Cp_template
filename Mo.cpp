8979cf54007917cef7c6b7655bc9879646576281
Kai <LINKAIZXC@GMAIL.COM>
2025-09-27 14:14:24 +0800

    Update Mo.cpp

--- Mo.cpp
+++ Mo.cpp
@@ -1,45 +1,101 @@
-void remove(idx);  // TODO: remove value at idx from data structure
-void add(idx);     // TODO: add value at idx from data structure
-int get_answer();  // TODO: extract the current answer of the data structure
+static inline long long hilbertOrder(int x, int y, int pow, int rot) {
+    if (pow == 0) return 0;
+    int hpow = 1 << (pow - 1);
+    int seg = (x < hpow) ? ((y < hpow) ? 0 : 3) : ((y < hpow) ? 1 : 2);
+    seg = (seg + rot) & 3;
+    static const int rotateDelta[4] = {3, 0, 0, 1};
+    int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
+    int nrot = (rot + rotateDelta[seg]) & 3;
+    long long sub = 1LL << (2 * pow - 2);
+    long long ans = seg * sub;
+    long long add = hilbertOrder(nx, ny, pow - 1, nrot);
+    ans += (seg == 1 || seg == 2) ? add : (sub - add - 1);
+    return ans;
+}
 
-int block_size;
+// ---- Problem-specific Mo state: odd/even frequencies in window ----
+static const int MAXV = 1'000'000;      // given: A[i] in [0, 1e6]
+static int cnts[MAXV + 1];              // frequency table : MAKE THIS GLOBAL TO IMPROVE TIME LIMIT
+static int oddKinds;                    // #values currently with odd frequency
 
 struct Query {
     int l, r, idx;
-    bool operator<(Query other) const
-    {
-        return make_pair(l / block_size, r) <
-               make_pair(other.l / block_size, other.r);
-    }
+    long long ord;                      // Hilbert order key
 };
 
-vector<int> mo_s_algorithm(vector<Query> queries) {
-    vector<int> answers(queries.size());
-    sort(queries.begin(), queries.end());
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    // TODO: initialize data structure
+    int T; 
+    if (!(cin >> T)) return 0;
+    while (T--) {
+        int n, q;
+        cin >> n >> q;
 
-    int cur_l = 0;
-    int cur_r = -1;
-    // invariant: data structure will always reflect the range [cur_l, cur_r]
-    for (Query q : queries) {
-        while (cur_l > q.l) {
-            cur_l--;
-            add(cur_l);
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i) cin >> a[i];
+
+        // Choose pow for hilbert: smallest pow s.t. 2^pow >= n
+        int pow = 0;
+        while ((1 << pow) < max(1, n)) ++pow;
+
+        vector<Query> qs;
+        qs.reserve(q);
+        for (int i = 0; i < q; ++i) {
+            int l, r; 
+            cin >> l >> r;
+            --l; --r;                   // to 0-indexed
+            if (l > r) swap(l, r);
+            long long ord = hilbertOrder(l, r, pow, 0);
+            qs.push_back({l, r, i, ord});
         }
-        while (cur_r < q.r) {
-            cur_r++;
-            add(cur_r);
+
+        // Sort by Hilbert order
+        sort(qs.begin(), qs.end(), [](const Query& A, const Query& B){
+            return A.ord < B.ord;
+        });
+
+        // Reset state for this test
+        memset(cnts, 0, sizeof(cnts));
+
+        // TODO: ADJUST DATA STRUCTURE HERE
+        oddKinds = 0;
+
+        // Mo pointers
+        int curL = 0, curR = -1;
+
+        // TODO: ADJUST ADD AND REMOVE ACCORDINGLY;
+        auto add = [&](int idx) {
+            cnts[a[idx]]++;
+            if (cnts[a[idx]] & 1) {
+                oddKinds++;
+            } else {
+                oddKinds--;
+            }
+        };
+        auto rem = [&](int idx) {
+            cnts[a[idx]]--;
+            if (cnts[a[idx]] & 1) {
+                oddKinds++;
+            } else {
+                oddKinds--;
+            }
+        };
+
+        vector<int> ans(q);
+        for (const auto& qu : qs) {
+            int L = qu.l, R = qu.r;
+            while (curL > L) add(--curL);
+            while (curR < R) add(++curR);
+            while (curL < L) rem(curL++);
+            while (curR > R) rem(curR--);
+
+            // YES if no odd counts in [L, R]
+            ans[qu.idx] = (oddKinds == 0) ? 1 : 0;
         }
-        while (cur_l < q.l) {
-            remove(cur_l);
-            cur_l++;
-        }
-        while (cur_r > q.r) {
-            remove(cur_r);
-            cur_r--;
-        }
-        answers[q.idx] = get_answer();
+
+        for (int v : ans) cout << (v ? "YES\n" : "NO\n");
     }
-    return answers;
+    return 0;
 }
