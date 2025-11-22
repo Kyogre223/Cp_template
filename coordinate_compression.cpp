0b9ee8d223398de03cdd868c53d93813f17c8b1c
Kai <LINKAIZXC@GMAIL.COM>
2025-11-22 21:38:04 +0800

    Update coordinate_compression.cpp

--- coordinate_compression.cpp
+++ coordinate_compression.cpp
@@ -1,34 +1,8 @@
-template <class T>
-struct Compressor {
-    vector<T> vals;     // sorted unique values
+    // 2) sort and unique
+    sort(vals.begin(), vals.end());
+    vals.erase(unique(vals.begin(), vals.end()), vals.end());
 
-    Compressor() = default;
-
-    // Build from an existing array
-    explicit Compressor(const vector<T>& a) {
-        vals = a;
-        sort(vals.begin(), vals.end());
-        vals.erase(unique(vals.begin(), vals.end()), vals.end());
-    }
-
-    // Add single value (if you want to build incrementally)
-    void add(T x) {
-        vals.push_back(x);
-    }
-
-    // Call after adding values manually
-    void build() {
-        sort(vals.begin(), vals.end());
-        vals.erase(unique(vals.begin(), vals.end()), vals.end());
-    }
-
-    // Get compressed index (0-based)
-    int get(T x) const {
-        return int(lower_bound(vals.begin(), vals.end(), x) - vals.begin());
-    }
-
-    // Optional: decompress index -> original value
-    T operator[](int i) const {
-        return vals[i];
-    }
-};
+    // 3) mapping function: original value -> compressed 1..K
+    auto get = [&](int x) {
+        return int(lower_bound(vals.begin(), vals.end(), x) - vals.begin()) + 1;
+    };
