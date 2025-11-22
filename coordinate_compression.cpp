49c40504bd6db20f1e0087b5c0951dd6eaf46d36
Kai <LINKAIZXC@GMAIL.COM>
2025-11-22 21:49:57 +0800

    Update coordinate_compression.cpp

--- coordinate_compression.cpp
+++ coordinate_compression.cpp
@@ -1,8 +1,6 @@
-    // 2) sort and unique
-    sort(vals.begin(), vals.end());
-    vals.erase(unique(vals.begin(), vals.end()), vals.end());
-
-    // 3) mapping function: original value -> compressed 1..K
-    auto get = [&](int x) {
-        return int(lower_bound(vals.begin(), vals.end(), x) - vals.begin()) + 1;
+    auto get = [&](long long x) -> int {
+        auto it = upper_bound(vals.begin(), vals.end(), x);
+        if (it == vals.begin()) return -1;
+        --it;
+        return int(it - vals.begin());
     };
