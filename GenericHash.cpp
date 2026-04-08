ae9e69f225bf73dfa81f1e3eb2aa3d0fe339ad3f
Kai <LINKAIZXC@GMAIL.COM>
2026-04-08 18:51:48 +0800

    Update GenericHash.cpp

--- GenericHash.cpp
+++ GenericHash.cpp
@@ -1,14 +1,12 @@
 #include <vector>
 #include <functional>
 
-// Generic Vector Hasher
+const size_t NULL_HASH = 0xDEADC0DE;  // for null conditions generically.
 struct VectorHasher {
     template <typename T>
     size_t operator()(const std::vector<T>& v) const {
         size_t seed = 0;
         for (const T& i : v) {
-            // This is the "Magic" hash_combine logic
-            // It prevents common collisions (like [1, 2] vs [2, 1])
             seed ^= std::hash<T>{}(i) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
         }
         return seed;
